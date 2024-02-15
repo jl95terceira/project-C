c96cdf11b9b79e4149ce674e4d3d22384222fcf3
Foo Bar <foo@foo.com>
2024-02-15 20:43:45 +0000

    fix album director name - replace illegal path characters with underscore

--- music.py
+++ music.py
@@ -5,6 +5,10 @@ import xml.etree.ElementTree as et
 from   urllib.request import urlopen as urlopen
 import bs4
 
+def patch_path(p:str):
+
+    return re.sub(pattern=r'[<>\:"/\\\|\?*]',repl='_',string=p)
+
 def descape(s:str):
 
     return re.sub(pattern='%([0-9]{2})',repl=lambda m: eval(f'\'\\x{m.group(1)}\''),string=s)
@@ -19,7 +23,7 @@ def get_album(url:str,
     doc            = bs4.BeautifulSoup(urlopen(url).read().decode(),features='html.parser')
     page_content   = doc         .find(id='rightColumn').find(id='pageContent')
     album_title    = page_content.find('h2').find(string=True,recursive=False)
-    album_dir      = os.path.join(dir,album_title)
+    album_dir      = os.path.join(dir,patch_path(album_title))
     os.makedirs(album_dir,exist_ok=True)
     for i,album_picture_link in enumerate(div.find('a')['href'] for div in page_content.find_all(attrs={'class':'albumImage'})):
 
@@ -43,7 +47,7 @@ def get_album(url:str,
         song_page    = bs4.BeautifulSoup(urlopen(re.match(pattern='https://[A-Za-z0-9.]+?.com',string=url).group(0)+track_link).read().decode(),features='html.parser')
         song_content = song_page.find(id='rightColumn').find(id='pageContent')
         song_link    = song_content.find(attrs={'class':'songDownloadLink'}).parent['href']
-        song_name    = os.path.join(album_dir,descape(urltail(song_link)))
+        song_name    = os.path.join(album_dir,patch_path(descape(urltail(song_link))))
         print(song_name)
         if os.path.exists(song_name): continue
         with open(song_name, 'wb') as f: 
